a757fb4a879703e599da75e291d7d456772a42a9
yazalim-silelim <mericefece09@gmail.com>
2025-09-02 17:11:47 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -5,10 +5,9 @@ const apps = {
     "icon": "https://f-droid.org/repo/de.danoeh.antennapod/en-US/icon_w44b41PyuNt3pI7Gh8zYHJrWgu__3HT7YSWZtttfenk=.png",
     "screenshots": ["https://via.placeholder.com/300x150?text=Antenna+1", "https://via.placeholder.com/300x150?text=Antenna+2"],
     "versions": [
-      { "version": "2.1.2", "apk": "https://f-droid.org/repo/AntennaPod_2.1.2.apk" },
-      { "version": "2.0.8", "apk": "https://f-droid.org/repo/AntennaPod_2.0.8.apk" },
-      { "version": "1.9.5", "apk": "https://f-droid.org/repo/AntennaPod_1.9.5.apk" },
-      { "version": "1.8.0", "apk": "https://f-droid.org/repo/AntennaPod_1.8.0.apk" }
+      { "version": "3.9.0", "apk": "https://f-droid.org/repo/de.danoeh.antennapod_3090095.apk" },
+      { "version": "3.8.0", "apk": "https://f-droid.org/repo/de.danoeh.antennapod_3080095.apk" },
+      { "version": "3.7.0", "apk": "https://f-droid.org/repo/de.danoeh.antennapod_3070095.apk" }
     ]
   },
   "newpipe": {
@@ -17,9 +16,10 @@ const apps = {
     "icon": "https://f-droid.org/repo/org.schabi.newpipe/en-US/icon_OHy4y1W-fJCNhHHOBCM9V_cxZNJJgbcNkB-x7UDTY9Q=.png",
     "screenshots": ["https://via.placeholder.com/300x150?text=NewPipe+1", "https://via.placeholder.com/300x150?text=NewPipe+2"],
     "versions": [
-      { "version": "0.23.6", "apk": "https://f-droid.org/repo/NewPipe_0.23.6.apk" },
-      { "version": "0.23.5", "apk": "https://f-droid.org/repo/NewPipe_0.23.5.apk" },
-      { "version": "0.23.4", "apk": "https://f-droid.org/repo/NewPipe_0.23.4.apk" }
+      { "version": "0.28.0", "apk": "https://f-droid.org/repo/org.schabi.newpipe_1005.apk" },
+      { "version": "0.27.7", "apk": "https://f-droid.org/repo/org.schabi.newpipe_1004.apk" },
+      { "version": "0.27.6", "apk": "https://f-droid.org/repo/org.schabi.newpipe_1003.apk" },
+      { "version": "0.27.5", "apk": "https://f-droid.org/repo/org.schabi.newpipe_1002.apk" }
     ]
   },
   "vlc": {
@@ -28,12 +28,9 @@ const apps = {
     "icon": "https://f-droid.org/repo/org.videolan.vlc/en-US/icon_yAfSvPRJukZzMMfUzvbYqwaD1XmHXNtiPBtuPVHW-6s=.png",
     "screenshots": ["https://via.placeholder.com/300x150?text=VLC+1", "https://via.placeholder.com/300x150?text=VLC+2"],
     "versions": [
-      { "version": "3.6.5", "apk": "https://f-droid.org/repo/VLC_3.6.5.apk" },
-      { "version": "3.6.4", "apk": "https://f-droid.org/repo/VLC_3.6.4.apk" },
-      { "version": "3.6.3", "apk": "https://f-droid.org/repo/VLC_3.6.3.apk" },
-      { "version": "3.6.2", "apk": "https://f-droid.org/repo/VLC_3.6.2.apk" },
-      { "version": "3.6.1", "apk": "https://f-droid.org/repo/VLC_3.6.1.apk" },
-      { "version": "3.5.0", "apk": "https://f-droid.org/repo/VLC_3.5.0.apk" }
+      { "version": "3.6.5", "apk": "https://f-droid.org/repo/org.videolan.vlc_13060508.apk" },
+      { "version": "3.6.4", "apk": "https://f-droid.org/repo/org.videolan.vlc_13060408.apk" },
+      { "version": "3.6.3", "apk": "https://f-droid.org/repo/org.videolan.vlc_13060305.apk" }
     ]
   }
 };
